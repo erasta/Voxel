b2468a35229b82e58639ba3949d901eaf8c81526
erasta <eran.geva@gmail.com>
2017-05-15 16:44:13 +0300

    center voxels, remove constants

--- src/VoxelRender.js
+++ src/VoxelRender.js
@@ -92,9 +92,7 @@ VoxelRender.vertShader = `
         vec3 down = vec3(0.0, 0.0, -1.0);
         vec4 quat = rotationBetweenVectorsToQuaternion(down, cameraVector);
 		vec3 rotpos = rotateVectorByQuaternion(position - center, quat) + center;
-        // worldSpaceCoords = (rotpos - firstCell) / cellSize / size + vec3(0.5); //move it from [-0.5;0.5] to [0,1]
-        worldSpaceCoords = (rotpos - firstCell) / cellSize / vec3(64.0); //move it from [-0.5;0.5] to [0,1]
-        // worldSpaceCoords = position + vec3(0.5);
+        worldSpaceCoords = (rotpos - firstCell) / cellSize / size; //move it from [-0.5;0.5] to [0,1]
         gl_Position = projectionMatrix * modelViewMatrix * vec4(rotpos, 1.0);
         // gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
